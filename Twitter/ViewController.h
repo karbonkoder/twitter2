65394e5d0cea49a0c34585e5cd630369282403a3
Ankush Raina <araina@groupon.com>
2015-11-08 13:43:05 -0800

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Ankush Raina on 11/8/15.
+//  Copyright © 2015 Ankush Raina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Ankush Raina on 11/8/15.
+//  Copyright © 2015 Ankush Raina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
